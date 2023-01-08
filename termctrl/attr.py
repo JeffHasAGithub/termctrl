0ff336376212227bcd8700034c6e646c37bb51c2
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-07 21:42:44 -0600

    Refactor color module (#4)
    * Rename color.py to attr.py
    
    * Rename test_color.py to test_termctrl.py
    
    * Update tests
    
    * Remove write_fg and write_bg functions
    
    * Add Format enum
    
    * Update doc strings

---
+++ termctrl/attr.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+
+class Format(Enum):
+    """ANSI Formatting attributes"""
+    RESET = 0
+    BRIGHT = 1
+    DIM = 2
+    UNDERSCORE = 4
+    BLINK = 5
+    REVERSE = 7
+    HIDDEN = 8
+
+
+class FgColor(Enum):
+    """ANSI foreground colors"""
+    BLACK = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    WHITE = 37
+
+
+class BgColor(Enum):
+    """ANSI background colors"""
+    BLACK = 40
+    RED = 41
+    GREEN = 42
+    YELLOW = 43
+    BLUE = 44
+    MAGENTA = 45
+    CYAN = 46
+    WHITE = 47
---
+++ tests/test_termctrl.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestTermCtrl(unittest.TestCase):
+    def test(self):
+        self.assertEqual(True, True)
