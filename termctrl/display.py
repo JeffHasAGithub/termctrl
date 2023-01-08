d26fb46613a62ec6c63f08ae4de46ae8e8ce74e1
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-07 20:08:09 -0600

    Add docs strings

--- termctrl/display.py
+++ termctrl/display.py
@@ -3,24 +3,30 @@ from .common import ANSI_ESC
 
 
 def clear_ln_from_cursor(buffer=sys.stdout):
+    """Clear line from the cursor to the end of the line"""
     buffer.write(f"{ANSI_ESC}[K")
 
 
 def clear_ln_to_cursor(buffer=sys.stdout):
+    """Clear line from beginning of the line up to the cursor"""
     buffer.write(f"{ANSI_ESC}[1K")
 
 
 def clear_ln(buffer=sys.stdout):
+    """Clear the current line"""
     buffer.write(f"{ANSI_ESC}[2K")
 
 
 def clear_up(buffer=sys.stdout):
+    """Clear screen from the current line up to the top of the screen"""
     buffer.write(f"{ANSI_ESC}[1J")
 
 
 def clear_dn(buffer=sys.stdout):
+    """Clear screen from the current line down to the bottom of the screen"""
     buffer.write(f"{ANSI_ESC}[J")
 
 
 def clear_screen(buffer=sys.stdout):
+    """Clear the entire screen"""
     buffer.write(f"{ANSI_ESC}[2J")
