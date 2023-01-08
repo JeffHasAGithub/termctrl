ab9f020f0e7972f8ac4f68544ff358894d856b3f
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-07 18:51:25 -0600

    Add functions for moving the cursor

---
+++ termctrl/cursor.py
@@ -0,0 +1,26 @@
+import sys
+from .common import ANSI_ESC
+
+
+def home(buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}[H")
+
+
+def move(row: int, col: int, buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}[{row};{col}H")
+
+
+def move_up(count=1, buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}{count}A")
+
+
+def move_dn(count=1, buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}{count}B")
+
+
+def move_fw(count=1, buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}{count}C")
+
+
+def move_bw(count=1, buffer=sys.stdout):
+    buffer.write(f"{ANSI_ESC}{count}D")
