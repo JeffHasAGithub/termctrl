0a50a6f45f33372472603825d85d8bef42f8e6eb
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-11 21:55:36 -0600

    Add erase method to TermCtrl class

--- termctrl/termctrl.py
+++ termctrl/termctrl.py
@@ -1,6 +1,7 @@
 import sys
 from .common import escape
-from .attr import Attr
+from .eraser import Erase, esc_erase
+from .attr import Attr, Format
 
 
 class TermCtrl():
@@ -13,12 +14,15 @@ class TermCtrl():
     def writeln(self, s: str):
         self.write(s + "\n")
 
+    def erase(self, e: Erase):
+        self.write(esc_erase(e))
+
     def set_attr(self, attr: Attr):
         attr = attr.value
         self.write(escape(f"[{attr}m"))
 
     def reset_attrs(self):
-        self.set_attr(Attr.RESET)
+        self.set_attr(Format.RESET)
 
     def reset_all(self):
         self.write(escape("c"))
