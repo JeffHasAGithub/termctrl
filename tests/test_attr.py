b181648cbcf55647bb70f036fb06a9bf99b93ef2
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-15 14:16:17 -0600

    Update tests.test_attr module

--- tests/test_attr.py
+++ tests/test_attr.py
@@ -1,8 +1,18 @@
 import unittest
-from termctrl.attr import esc_attrs
+from termctrl.attr import Attr, esc_attrs
+from typing import NamedTuple
+
+
+class TCase(NamedTuple):
+    text: str
+    attrs: tuple[Attr]
+    want: str
 
 
 class TestAttr(unittest.TestCase):
     def test_esc_attr(self):
-        got = esc_attrs("")
-        self.assertEqual("", got)
+        cases = {"empty_string": TCase("", (), "")}
+
+        for case in cases.values():
+            got = esc_attrs(case.text, *case.attrs)
+            self.assertEqual(got, case.want)
