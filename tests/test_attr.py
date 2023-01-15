501c158057f750f7bbff3e03a17b473b9ed152f2
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-15 14:30:47 -0600

    Update test_esc_attr method in tests.test_attr.TestAttr class

--- tests/test_attr.py
+++ tests/test_attr.py
@@ -1,5 +1,6 @@
 import unittest
-from termctrl.attr import Attr, esc_attrs
+from termctrl.attr import (Attr, FgColor,
+                           BgColor, Format, esc_attrs)
 from typing import NamedTuple
 
 
@@ -11,8 +12,12 @@ class TCase(NamedTuple):
 
 class TestAttr(unittest.TestCase):
     def test_esc_attr(self):
-        cases = {"empty_string": TCase("", (), "")}
+        cases = (TCase("", (), ""),
+                 TCase("Hello!", (FgColor.RED,),
+                       "\033[0;31mHello!\033[0m"),
+                 TCase("Hello!", (FgColor.GREEN, Format.BLINK),
+                       "\033[0;32;5mHello!\033[0m"))
 
-        for case in cases.values():
+        for case in cases:
             got = esc_attrs(case.text, *case.attrs)
             self.assertEqual(got, case.want)
