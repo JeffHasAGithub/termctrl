1246ac32eacda49b56eb5d8079bd69ed61978ade
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-14 19:43:25 -0600

    Refactor termctrl.cursor module

--- termctrl/cursor.py
+++ termctrl/cursor.py
@@ -1,42 +1,16 @@
-import sys
-from .common import ANSI_ESC
+from .common import escape
 
 
-def home(buffer=sys.stdout):
-    """Reset cursor position to top-left corner of terminal"""
-    buffer.write(f"{ANSI_ESC}[H")
-
-
-def move(row: int, col: int, buffer=sys.stdout):
+def esc_move(row: int, col: int) -> str:
     """Move cursor to given row and col within terminal"""
-    buffer.write(f"{ANSI_ESC}[{row};{col}H")
+    return escape(f"[{row};{col}H")
 
 
-def move_up(count=1, buffer=sys.stdout):
-    """Move cursor up {count} number of rows within terminal"""
-    buffer.write(f"{ANSI_ESC}[{count}A")
-
-
-def move_dn(count=1, buffer=sys.stdout):
-    """Move cursor down {count} number of rows within terminal"""
-    buffer.write(f"{ANSI_ESC}[{count}B")
-
-
-def move_fw(count=1, buffer=sys.stdout):
-    """Move cursor forward {count} number of cols within terminal"""
-    buffer.write(f"{ANSI_ESC}[{count}C")
-
-
-def move_bw(count=1, buffer=sys.stdout):
-    """Move cursor backward {count} number of cols within terminal"""
-    buffer.write(f"{ANSI_ESC}[{count}D")
-
-
-def save(buffer=sys.stdout):
+def esc_save() -> str:
     """Save current cursor position"""
-    buffer.write(f"{ANSI_ESC}[s")
+    return escape("[s")
 
 
-def restore(buffer=sys.stdout):
+def esc_restore():
     """Restore cursor to last saved position"""
-    buffer.write(f"{ANSI_ESC}[u")
+    return escape("[u")
