f8ee9a6d8ca9fdc9834ddb5193fc4368e1522bc3
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-23 20:41:16 -0600

    Add parse_position function to termctrl.cursor module

--- termctrl/cursor.py
+++ termctrl/cursor.py
@@ -25,3 +25,20 @@ def esc_restore():
 
 def esc_query_position() -> str:
     return escape("[6n")
+
+
+def parse_position(pos: str) -> Position:
+    pre, pos, suf = pos[:3], pos[3:-1], pos[-1]
+
+    if pre != "^[[" or suf != "R":
+        raise CursorError(f"Cursor error: {pos} is an invalid cursor query")
+
+    row, col = pos.split(";")
+    try:
+        row = int(row)
+        col = int(col)
+    except ValueError:
+        raise CursorError(f"Cursor error: {row}, {col} cannot be converted"
+                          " to an integer")
+
+    return Position(row, col)
