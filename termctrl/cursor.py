89eaa1bb969a6fa9204798e5126819aae284f6c7
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-23 21:03:24 -0600

    Update cursor module (#11)
    * Add esc_position function to termctrl.cursor module
    
    * Rename esc_position func in termctrl.cursor to esc_query_position
    
    * Add Position class to termctrl.cursor module
    
    * Add CursorError class to termctrl.error module
    
    * Update imports in termctrl.error module
    
    * Add parse_position function to termctrl.cursor module
    
    * Add test_cursor module to tests package
    
    * Add TestCursor class to tests.test_cursor module
    
    * Add test_parse_position_valid method to tests.test_cursor.TestCursor class
    
    * Add test_parse_position_bad_pre method to tests.test_cursor.TestCursor class
    
    * Add test_parse_position_bad_suf method to tests.test_cursor.TestCursor class
    
    * Add test_parse_position_bad_row method to tests.test_cursor.TestCursor class

--- termctrl/cursor.py
+++ termctrl/cursor.py
@@ -1,4 +1,11 @@
+from typing import NamedTuple
 from .common import escape
+from .error import CursorError
+
+
+class Position(NamedTuple):
+    row: int
+    col: int
 
 
 def esc_move(row: int, col: int) -> str:
@@ -14,3 +21,24 @@ def esc_save() -> str:
 def esc_restore():
     """Restore cursor to last saved position"""
     return escape("[u")
+
+
+def esc_query_position() -> str:
+    return escape("[6n")
+
+
+def parse_position(pos: str) -> Position:
+    pre, pos, suf = pos[:3], pos[3:-1], pos[-1]
+
+    if pre != "^[[" or suf != "R":
+        raise CursorError(f"Cursor error: {pos} is an invalid cursor query")
+
+    row, col = pos.split(";")
+    try:
+        row = int(row)
+        col = int(col)
+    except ValueError:
+        raise CursorError(f"Cursor error: {row}, {col} cannot be converted"
+                          " to an integer")
+
+    return Position(row, col)
---
+++ tests/test_cursor.py
@@ -0,0 +1,32 @@
+import unittest
+from termctrl.cursor import Position, parse_position
+from termctrl.error import CursorError
+
+
+class TestCursor(unittest.TestCase):
+    def test_parse_position_valid(self):
+        arg = "^[[37;1R"
+
+        want = Position(37, 1)
+        got = parse_position(arg)
+
+        self.assertEqual(want.row, got.row)
+        self.assertEqual(want.col, got.col)
+
+    def test_parse_position_bad_pre(self):
+        arg = "^[12;24R"
+
+        with self.assertRaises(CursorError):
+            parse_position(arg)
+
+    def test_parse_position_bad_suf(self):
+        arg = "^[[17;1r"
+
+        with self.assertRaises(CursorError):
+            parse_position(arg)
+
+    def test_parse_position_bad_row(self):
+        arg = "^[[Hi;7R"
+
+        with self.assertRaises(CursorError):
+            parse_position(arg)
